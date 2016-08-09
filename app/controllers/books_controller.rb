7395e19c2029fff8e6715e94cf5cff4050873828
SilverFox70 <wfbrinkert@gmail.com>
2016-08-09 16:58:44 -0700

    Edit books form; categories addable from book form now

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -41,8 +41,26 @@ class BooksController < ApplicationController
   # PATCH/PUT /books/1
   # PATCH/PUT /books/1.json
   def update
+    bp = book_params
+    categories = Category.where(:id => book_params[:category_ids]).pluck(:name)
+    p "---------------- category list -----------------"
+    categories.each do |cat|
+      p cat
+    end
+    if categories.include?("New Category")
+      new_category = Category.new(name: book_params[:other_cat])
+      if new_category.save
+        flash[:message] = "New Category Added."
+        p "-------------------- adding category ----------------"
+        p "new cat id: #{new_category.id}"
+        p "old cat id: #{Category.find_by(name: "New Category").id}"
+        bp[:category_ids].delete("#{Category.find_by(name: "New Category").id}")
+        bp[:category_ids] << "#{new_category.id}"
+        p "bp: #{bp[:category_ids]}"
+      end
+    end
     respond_to do |format|
-      if @book.update(book_params)
+      if @book.update(bp)
         format.html { redirect_to @book, notice: 'Book was successfully updated.' }
         format.json { render :show, status: :ok, location: @book }
       else
@@ -70,6 +88,6 @@ class BooksController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def book_params
-      params.require(:book).permit(:isbn, :title, :year_published, :author, :category_ids => [])
+      params.require(:book).permit(:isbn, :title, :year_published, :author, :other_cat, :category_ids => [])
     end
 end
